d60c6b84a5fa1b4de77ea0538375c2b652957efe
Jackson Williams <jacksonwilliams.williams@gmail.com>
2025-01-30 17:43:15 -0700

    generate

--- bambootest/bamboohrhackerranktest.py
+++ bambootest/bamboohrhackerranktest.py
@@ -1,11 +1,55 @@
-def PrintNumbers(n):
-    output_array = []
-    for i in range (1, n + 1):
-        output_array.append(i)
-    print(output_array)
+#!/bin/python3
 
-    The first thing I would want to know is what the customer did that brought the issue to light. I would ask them to explain the exact steps they took that led them to the problem. From there I would look to see if there were any crash reports or error logs or anything like that. Then I would try to reproduce the issue with the same conditions that the customer was using, like OS, settings, data, browser, etc while watching the error logs. Once we are able to reproduce the issue, I would take a close look at all the code in and surrounding the area. Is this an edge case scenario where it's only happening with specific input types, at certain times, etc, or is this a seldom used part of the application that hasn't been maintained? I would check to see if any third-party libraries or APIs have changed/are functioning properly? I would comment out certain blocks of code and run the application to see if the problems are still there. 
+import math
+import os
+import random
+import re
+import sys
+import json
 
-Once we know where the problems are coming from I would follow the data/code back to the root cause and find a fix for that. I would then review the unit tests that we have associated for that, and implement the changes that were just made to fix the problem, I would also want to try to trigger the same issue that was arising to see if the unit tests catch it this time. From there we would start regression testing to make sure we didn't break anything else while fixing this problem. Then we would roll out the fix to a staging environment, make sure that works and is going well, then we would release it to prod. 
 
-I would keep detailed accounts of what the problem was and how it occurred, and how we fixed it, to help the team in case something similar happens again.  
+#
+# Complete the 'generateJSON' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts 2D_STRING_ARRAY lines as parameter.
+#
+
+
+def generateJSON(lines):
+    # Write your code here
+    result = []
+    for item in lines:
+        
+        # Check for empty input
+        if item[1] == "": 
+            continue
+        
+        # Convert manager_id to JSON NULL if empty
+        if item[2] == "":
+            manager_id = None
+        else:
+            manager_id = int(item[2])
+            
+        # Transform array to dictionary
+        result.append({
+            "id": int(item[0]),
+            "name": item[1],
+            "manager_id": manager_id
+        })
+        
+    return json.dumps(result, separators=(',', ':'))
+    
+
+print(generateJSON([
+  [
+    "1",
+    "Gregory Gwilliam",
+    "2"
+  ],
+  [
+    "2",
+    "Brice Martin",
+    ""
+  ]
+]))
